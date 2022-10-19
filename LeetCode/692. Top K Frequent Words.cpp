1c0aaeaf730792ad6ddef62110ba34a26437aa44
atiQue <atiqurrahman7465@gmail.com>
2022-10-19 13:25:48 +0600

    new file:   LeetCode/692. Top K Frequent Words.cpp

---
+++ LeetCode/692. Top K Frequent Words.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string> &words, int k) {
+        unordered_map<string, int> mp;
+        int len = words.size();
+        for (int i = 0; i < len; i++) {
+            mp[words[i]]++;
+        }
+
+        vector<pair<string, int>> v;
+        for (auto const &[key, val]: mp) {
+            v.push_back({key, val});
+        }
+
+        sort(v.begin(), v.end(), compare);
+
+        vector<string> result;
+        for (int i = 0; i < k; i++) {
+            result.push_back(v[i].first);
+        }
+
+        return result;
+    }
+
+    static bool compare(pair<string, int> p1, pair<string, int> p2) {
+        if (p1.second != p2.second) {
+            return p1.second > p2.second;
+        } else if (p1.first.compare(0, p2.first.size(), p2.first) == 0 ||
+                   p2.first.compare(0, p1.first.size(), p1.first) == 0) {
+            return p1.first.size() < p2.first.size();
+        } else
+            return p1.first < p2.first;
+    }
+
+};
