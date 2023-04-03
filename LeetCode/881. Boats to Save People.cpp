370b65338cc59007915439cf231764ad3b507cc6
atiQue <atiqurrahman7465@gmail.com>
2023-04-03 09:49:26 +0600

    new file:   LeetCode/881. Boats to Save People.cpp

---
+++ LeetCode/881. Boats to Save People.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int i = 0, j = people.size() - 1, cnt = 0;
+        while(i <= j){
+            if(people[i] + people[j] > limit) j--;
+            else {i++; j--;}
+            cnt++;
+        }
+        return cnt;
+    }
+};
